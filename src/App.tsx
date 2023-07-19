2554dbb28a04f83038b363e326c3309b9c15cc0c
오현재 <55138387+hangooksaram@users.noreply.github.com>
2023-07-19 22:21:43 +0900

    FEAT:메인 화면 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import Search from "./components/Search";
+import Result from "./components/Result/Result";
+import useSearch from "./hooks/useSearch";
+import styled from "@emotion/styled";
 
 function App() {
-  return <div className="App"></div>;
+  const { searchValue, setSearchValue, debouncedValue, searchResults } =
+    useSearch();
+  return (
+    <Container className="App">
+      <Title>국내 모든 임상시험 검색하고 온라인으로 참여하기</Title>
+      <Search
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+        searchQuery={debouncedValue}
+      />
+      <Result searchQuery={debouncedValue} searchResults={searchResults} />
+    </Container>
+  );
 }
+
+const Title = styled.h2({
+  fontSize: "2.125rem",
+  fontWeight: 700,
+  lineHeight: "1.6",
+  textAlign: "center",
+});
+
+const Container = styled.main({
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  width: "45%",
+  margin: "0 auto",
+  paddingTop: "70px",
+});
 
 export default App;
